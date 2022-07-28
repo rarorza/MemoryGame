8daf888fcb839d05ab5822258d49cc0132c20ae7
rarorza <rarorza@protonmail.com>
2022-07-28 07:14:54 -0300

    update

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,12 +6,14 @@ const validateInput = ({ target }) => {
   if (target.value.length > 2) {
     button.removeAttribute('disabled');
     return;
-  };
+  }
+
   button.setAttribute('disabled', '');
 }
 
 const handleSubmit = (event) => {
   event.preventDefault();
+
   localStorage.setItem('player', input.value);
   window.location = 'pages/game.html';
 }
